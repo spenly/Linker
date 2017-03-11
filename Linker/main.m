f86fc4b6f97c8ba6b65e67a3ec39abdf5065429d
spenly.jia <zhongxian.jia@gemii.cc>
2017-03-12 00:33:06 +0800

    Initial Commit

---
+++ Linker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Linker
+//
+//  Created by spenly.jia on 2017/3/12.
+//  Copyright © 2017年 spenly.jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
